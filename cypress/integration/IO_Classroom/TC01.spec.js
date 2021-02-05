fad750f0f1083395983a84bf185883107b01a40d
asaifbcone <77141868+asaifbcone@users.noreply.github.com>
2021-02-05 17:29:22 +0530

    update the framework (#3)

--- cypress/integration/IO_Classroom/TC01.spec.js
+++ cypress/integration/IO_Classroom/TC01.spec.js
@@ -6,13 +6,13 @@
 import login from '../Pageobject/login.js'
 
 
-
+const log_in = new login()
 
 beforeEach( () => {
-    let user
-    cy
-        .intercept('POST','https://www.skedula.com/').as('login')
-        .intercept('POST', 'https://auth.ioeducation.com/users/sign_in').as('loginDetail')
+    //let user
+    //cy
+      //  .intercept('POST','https://www.skedula.com/').as('login')
+       // .intercept('POST', 'https://auth.ioeducation.com/users/sign_in').as('loginDetail')
           
        // .intercept('POST', '/api/api/Account/Logoff').as('logOff')
 
@@ -31,59 +31,44 @@ beforeEach( () => {
 
 })
 
+afterEach(() => {
+    // runs after each test in the block
+    log_in.logout()
+  })
+
 
 describe('Login', function(){
-    let cypress
-    const log_in = new login()
-        it('Sign in', function(){
+    //let cypress
+    
+        it('Login to school', function(){
 
             cy
             .get('@user')
             .then(user => {
+                 log_in.login(user.email,user.password)
+                 log_in.proceed_school()
                  
-        log_in.signInButton().click()
+            })
+        })
+        it('Login to admin portal', function(){
 
-        log_in.email().type(user.email)
-         
-        log_in.signInButton().click()
+            cy
+            .get('@user')
+            .then(user => {
+                 log_in.login(user.email,user.password)
+                 log_in.proceed_admin()
+            })
+        })
+        it('Login to impersonate', function(){
 
-        log_in.password().type(user.password)
-
-        log_in.signInButton().click()
+            cy
+            .get('@user')
+            .then(user => {
+                 log_in.login(user.email,user.password)
+                 log_in.proceed_impersonate()
+            })
+       })
 
     
-        
-
-       // cy.visit('https://www.skedula.com/home')
-       
-        cy.visit(Cypress.env('baseUrlP'))
-
-
-            })
-      
-    })
 })
 
-// describe('GURU: Login Positive', () => {
-//     const log_in = new login()
-//     it('Positive Login', ()=>{
-//         cy
-//         .get('@user')
-//         .then(user => {
-//             log_in.signInButton().click()
-//             log_in.email().type(user.email)
-//             log_in.signInButton().click()
-//             log_in.password().type(user.password)
-//             log_in.signInButton().click()
-//         cy.wait('@login')
-//         .then(loginReq => {
-//             expect(loginReq.status).to.eq(200);
-//         });
-//         cy.wait('@loginDetail')
-//         .then(loginDetail => {
-//             expect(loginDetail.status).to.eq(200);
-//         });
-      
-//         });
-//     });
-// });
--- cypress/integration/Pageobject/login.js
+++ cypress/integration/Pageobject/login.js
@@ -1,13 +1,80 @@
 class login{
     
-    email(){
+    email_l(){
         return cy.get('#user_username')
     }
-    password(){
+    password_l(){
         return cy.get('#user_password')
     }
-    signInButton(){
+    signInButton_l(){
         return cy.get('#sign_in')
     }
+    logout_l(){
+        return cy.get('[href="/logout.aspx"]')
+    }
+
+    choose_school_l(){
+        return cy.get('#DBN_chzn > .chzn-single > span')
+    }
+
+    choose_term_l(){
+
+        return cy.get('#Term_chzn > .chzn-single > span')
+    }
+
+    login_school_l(){
+        return cy.get('#loginSKD > .ui-button-text')
+    }
+
+    login_admin_l(){
+        return cy.get('#loginAdmin > .ui-button-text')
+    }
+
+    login_impersonate_l(){
+        return cy.get('#loginTrouble > .ui-button-text')
+    }
+
+    login_user_l(){
+        return cy.get('.ui-button-text')
+    }
+
+    login(user,pwd){
+       this.signInButton_l().click()
+       this.email_l().type(user)
+       this.signInButton_l().click()
+       this.password_l().type(pwd)
+       this.signInButton_l().click()
+       
+
+    }
+
+    logout(){
+        this.logout_l().click()
+    }
+
+    proceed_school(){
+        this.choose_school_l()
+        this.choose_term_l()
+        this.login_school_l().click()
+    }
+
+    proceed_admin(){
+        this.choose_school_l()
+        this.choose_term_l()
+        this.login_admin_l().click()
+    }
+
+    proceed_impersonate(){
+        this.choose_school_l()
+        this.choose_term_l()
+        this.login_impersonate_l().click()
+    }
+
+    proceed_user(){
+        this.choose_school_l()
+        this.choose_term_l()
+        this.login_user_l.click()
+    }
+
 }
 export default login
