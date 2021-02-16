69b18886db0664306aa1dad2a84602967e2cf4f1
asaifbcone <asaif-c@illuminateed.net>
2021-02-16 15:55:40 +0530

    update the framework to work with all the browsers

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,47 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("sessionLogin", ()=>{
+    
+    // despite some information in cypress docs saying this should really
+    // never need to be called - this whole pyramid doesn't seem to work
+    // if it's not called
+    cy.fixture('user.json').as('user')
+    cy.clearCookies();
+
+    cy.request({
+        url: "https://www.skedula.com/",
+        headers: {
+            // send a fake cookie to ensure site sends back a fresh session id
+            cookie: "ASP.NET_SessionId=x"
+        }
+    })
+      .then(resp => {
+        let sessionId = resp.headers.["set-cookie"][0].split(";")[0].split("=")[1];
+        cy.get('@user').then(user => {
+        cy.request("POST", `https://auth.ioeducation.com/api/v1/auth/authorize`, {
+            username: user.email,
+            password: user.password,
+            client_id: "skedula_id",
+            state: sessionId
+            })
+        }).then(resp => {
+            cy.request(`https://www.skedula.com/login.aspx?code=${resp.body.code}&state=${resp.body.state}`).then(resp => {
+                cy.setCookie("ASP.NET_SessionId", sessionId);
+
+                cy.request({
+                    method: "POST",
+                    url: `https://www.skedula.com/auth/login/loginSkedula.aspx`,
+                    form: true,
+                    body: {
+                        DBN: "00X001",
+                        Term: "112"
+                    }
+                }).then(resp => {
+                    cy.log("login complete");
+                });
+            });
+        });
+      })
+});
